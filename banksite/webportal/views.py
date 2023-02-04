6e00aeba22c02f25bfc56174920ffab91dcc8a6c
Aritra Sarkar <aritra.sarkar@bajajfinserv.in>
2023-02-04 19:02:25 +0000

    Create Django Application

---
+++ banksite/webportal/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello, world. You're at the webportal index.")
